0d03d64e79e7d3f661dd8d919edd70de7112ccee
Raghad-Alahmadi <145593793+Raghad-Alahmadi@users.noreply.github.com>
2025-02-28 20:49:09 +0300

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmazonSa.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Pending";
+        public List<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+    }
+}
---
+++ Models/OrderDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace AmazonSa.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
